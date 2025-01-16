93d5a99056ea5a7c17e8a8c1504365ed04416455
Rudapaul <95488376+Rudapaul@users.noreply.github.com>
2025-01-16 09:29:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,65 +2,6 @@ import streamlit as st
 from fpdf import FPDF
 import pandas as pd
 
-# Hlavní data
-hlavni_data = {
-    "Adresa objektu": "Ve smečkách 592/22",
-    "PSČ": "11000 Praha 1",
-    "Náklady na spotřebu tepla na ÚT (Kč)": 719656,
-    "Náklady na spotřebu tepla na výrobu TV (Kč)": 217326,
-    "Náklady na spotřebu vody na výrobu TV (Kč)": 104108,
-    "Náklady na spotřebu studené vody (Kč)": 249836,
-    "Náklady celkem (Kč)": 1290926
-}
-
-# Data o bytech
-data_byty = [
-    {"Číslo bytu": 1, "Poloha": "4P/L", "Jméno bytu": "Ilona", "Plocha m2": 93},
-    {"Číslo bytu": 2, "Poloha": "PK/L", "Jméno bytu": "Brada", "Plocha m2": 39},
-    {"Číslo bytu": 3, "Poloha": "PK/S", "Jméno bytu": "Taras", "Plocha m2": 68},
-    {"Číslo bytu": 4, "Poloha": "PK/PO", "Jméno bytu": "Natty", "Plocha m2": 56},
-    {"Číslo bytu": 5, "Poloha": "1P/PO", "Jméno bytu": "Joga 1 patro", "Plocha m2": 62},
-    {"Číslo bytu": 6, "Poloha": "3P/PO", "Jméno bytu": "Šatny 3p", "Plocha m2": 45},
-    {"Číslo bytu": 7, "Poloha": "3P/PO", "Jméno bytu": "Joga 3p malá", "Plocha m2": 73},
-    {"Číslo bytu": 8, "Poloha": "3P/PO", "Jméno bytu": "Joga 3p do ulice", "Plocha m2": 122},
-    {"Číslo bytu": 9, "Poloha": "3P/L", "Jméno bytu": "Joga 3p velká", "Plocha m2": 303},
-    {"Číslo bytu": 10, "Poloha": "3P/L", "Jméno bytu": "Kancl 3p na levo", "Plocha m2": 42},
-    {"Číslo bytu": 11, "Poloha": "2P/S", "Jméno bytu": "2 patro do ulice", "Plocha m2": 166},
-    {"Číslo bytu": 12, "Poloha": "1P/S", "Jméno bytu": "Studio", "Plocha m2": 696},
-    {"Číslo bytu": 13, "Poloha": "PR/L", "Jméno bytu": "Little Bali", "Plocha m2": 343},
-    {"Číslo bytu": 14, "Poloha": "PR/PO", "Jméno bytu": "Kytky", "Plocha m2": 111},
-    {"Číslo bytu": 15, "Poloha": "2P/PO", "Jméno bytu": "2patro do zahrady", "Plocha m2": 116},
-    {"Číslo bytu": 16, "Poloha": "PR/PO", "Jméno bytu": "David", "Plocha m2": 457},
-    {"Číslo bytu": 17, "Poloha": "4P/PO", "Jméno bytu": "Michal", "Plocha m2": 128},
-    {"Číslo bytu": 18, "Poloha": "1P/PO", "Jméno bytu": "Babča", "Plocha m2": 110}
-]
-
-# Odečty tepla
-odecty_tepla = [
-    {"Číslo bytu": 1, "Typ a velikost radiátoru": "22/500x1000", "VČ-radio": 33834458, "Typ média": "HCA", "Aktuální hodnota": 1048},
-    {"Číslo bytu": 2, "Typ a velikost radiátoru": "22/600x1000", "VČ-radio": 33834487, "Typ média": "HCA", "Aktuální hodnota": 415},
-    {"Číslo bytu": 3, "Typ a velikost radiátoru": "22/600x600", "VČ-radio": 4530926, "Typ média": "HCA", "Aktuální hodnota": 734},
-    # Přidat zbytek odečtů z tepla zde dle vašich údajů...
-]
-
-# Odečty vody
-odecty_vody
-
-Rozumím, že máte pochybnosti, protože se zatím nepodařilo dodat plně funkční kód v jednom kroku. Teď se opravdu soustředím na to, aby byl kód **kompletní**, zahrnoval **všechna vaše data**, a fungoval **od začátku do konce** – včetně generování protokolů a zahrnutí všech detailů o bytech, odečtech tepla i vody.
-
-Nyní vytvořím kód, který splní vaše požadavky od A do Z, a pokud narazíte na jakýkoli problém, můžete mi ho hned ukázat, abych ho opravil.
-
-Připravím ho právě teď.
-
-Tady je kompletní kód aplikace, který zahrnuje všechna požadovaná data, zobrazuje je s možností úpravy a generuje PDF protokol:
-
-### Kód
-
-```python
-import streamlit as st
-from fpdf import FPDF
-import pandas as pd
-
 # Nastavení hlavních dat
 hlavni_data = {
     "Adresa objektu": "Ve smečkách 592/22",
